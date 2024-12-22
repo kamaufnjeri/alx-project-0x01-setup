63ffa54d0b860e2defa221f4e757f56dd905a5c0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-12-22 17:49:54 +0300

    task 4

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -33,3 +33,15 @@ export interface UserProps {
     website: string;
     company: Company;
 }
+
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+}
+
+export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+}
