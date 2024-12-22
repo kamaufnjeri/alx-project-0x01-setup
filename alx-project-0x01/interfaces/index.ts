92334f47e68cde99a0749e3616421286154b640b
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-12-22 17:43:44 +0300

    task 3

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,3 +4,32 @@ export interface PostProps {
     title: string;
     body: string;
 }
+
+interface Geo {
+    lat: string;
+    lng: string;
+}
+interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
+export interface UserProps {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+}
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,14 +1,38 @@
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () => {
-    return (
-      <div>
-        <Header/>
-        <h1 className="text-4xl">Users</h1>
-        <p className="mt-4">List of users will appear here.</p>
-      </div>
-    );
+const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+  console.log(users)
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">User Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+        {users.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
+
+  return {
+    props: {
+      users
+    }
   }
-  
-  export default Users;
-  
+}
+
+export default Users;
